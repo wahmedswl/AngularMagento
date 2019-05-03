e514566787f0527c10cb9c6ac60cddd8c43b15d3
Andrey Solovyov <44004249+GitGudLater@users.noreply.github.com>
2019-05-03 13:43:46 +0300

    Add files via upload

---
+++ catalog.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+const httpOptions = {
+  headers: new HttpHeaders({ 
+    'Access-Control-Allow-Origin':'*',
+    'Authorization':'authkey',
+    'userid':'1'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogService {
+
+  constructor(private http:HttpClient) {
+   }
+
+   getProducts(){
+     return this.http.get("http://localhost:8080/magento/rest/all/V1/products?searchCriteria=all?accept:application/json");
+   }
+   ///accept:application/json
+}
---
+++ product.ts
@@ -0,0 +1,13 @@
+export class Product{
+    public id:number;
+    public sku:string;
+    public name:string;
+    public attribute_set_id:number;
+    public price:number;
+    public status:number;
+    public visibility:number;
+    public type_id:string;
+    public created_at:string;
+    public updated_at:string;
+    //find picture atribute
+}
